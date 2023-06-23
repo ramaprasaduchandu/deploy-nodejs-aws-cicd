fcccb5d5c8f1ca51c4910400c8a25bf79b837991
Ramaprasadu chandu <Ramaprasaduchandu@git.com>
2023-06-23 16:56:06 +0530

    added community

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const {PORT = 3000} = process.env;
 
 console.log('PORT', PORT)
 
-app.get('/', (req, res) => res.send('Hello aws developers '))
+app.get('/', (req, res) => res.send('Hello aws developers community'))
 
 app.listen(PORT, () => 
    console.log(`Example app listening at http://localhost:${PORT}`))
